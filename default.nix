3e5411f88fb71abfff632482dfd30e2d978215b8
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-11-05 20:33:46 -0800

    Version 1.0.0 → 1.0.1

--- default.nix
+++ default.nix
@@ -1,15 +1,15 @@
-{ mkDerivation, base, foldl, managed, neat-interpolation
-, optparse-applicative, stdenv, text, turtle
+{ mkDerivation, base, bytestring, foldl, managed
+, neat-interpolation, optparse-applicative, stdenv, text, turtle
 }:
 mkDerivation {
   pname = "nix-delegate";
-  version = "1.0.0";
+  version = "1.0.1";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base foldl managed neat-interpolation optparse-applicative text
-    turtle
+    base bytestring foldl managed neat-interpolation
+    optparse-applicative text turtle
   ];
   executableHaskellDepends = [ base ];
   description = "Convenient utility for distributed Nix builds";
